10252b747bc38de5dedb8131cd75d01552dffc67
Cristian Bulgarelli <cristian.bulgarelli@overit.it>
2023-02-02 12:22:54 +0100

    second commit - project creation

--- src/main/java/com/example/changelogautomaticgeneration/ChangelogAutomaticGenerationApplication.java
+++ src/main/java/com/example/changelogautomaticgeneration/ChangelogAutomaticGenerationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChangelogAutomaticGenerationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChangelogAutomaticGenerationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChangelogAutomaticGenerationApplication.class, args);
+    }
 
 }
