ae1acf106d20a06bdd749a36a664ad6920d52cf8
Cristian Bulgarelli <cristian.bulgarelli@overit.it>
2023-02-02 11:57:20 +0100

    first commit

---
+++ src/main/java/com/example/changelogautomaticgeneration/ChangelogAutomaticGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.example.changelogautomaticgeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangelogAutomaticGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangelogAutomaticGenerationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/changelogautomaticgeneration/ChangelogAutomaticGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.changelogautomaticgeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangelogAutomaticGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
