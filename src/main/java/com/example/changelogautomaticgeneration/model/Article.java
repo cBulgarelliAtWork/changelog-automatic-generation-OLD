c80e40d632888e7c045a8310470e39753a078d08
Cristian Bulgarelli <cristian.bulgarelli@overit.it>
2023-02-03 10:41:43 +0100

    feat: article model class

---
+++ src/main/java/com/example/changelogautomaticgeneration/model/Article.java
@@ -0,0 +1,24 @@
+package com.example.changelogautomaticgeneration.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "article")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
+public class Article implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name = "description", unique = true, nullable = false, length = 256)
+    @NotNull
+    private String description;
+}
